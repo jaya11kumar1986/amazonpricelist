c25ae231dc3e70121fdb6a6e2f2f73b712618b9e
Jaya11kumar1986 <jai11sri1986@gmail.com>
2024-07-20 15:56:00 +0530

    New Java File added

---
+++ amazon/src/test/java/amazonprice/GoogleOpen.java
@@ -0,0 +1,18 @@
+package amazonprice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class GoogleOpen {
+
+	public static void main(String[] args) {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+
+	}
+
+}
