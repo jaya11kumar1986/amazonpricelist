595d24f55f0dcff62b671de52d464a8bf5995203
Jaya11kumar1986 <jai11sri1986@gmail.com>
2024-07-22 17:00:44 +0530

    Third

--- amazon/src/test/java/amazonprice/GoogleOpen.java
+++ amazon/src/test/java/amazonprice/GoogleOpen.java
@@ -2,16 +2,19 @@ package amazonprice;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class GoogleOpen {
-
-	public static void main(String[] args) {
+@Test
+	public void tearUp() {
 		
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
+		
+		driver.get("https://www.google.com/");
 
 	}
 
